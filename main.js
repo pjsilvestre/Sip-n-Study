d224c051ea933c46dc6ac3501d4de44635baf51d
Mohammad Sleiman <modysleiman@gmail.com>
2019-03-26 01:18:03 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,29 @@ function initMap() {
     zoom: 16
   });
 
-  //hardcoded markers for demo purposes
+     var SJSUstudentMarker = new google.maps.Marker({
+    position: {lat: 37.3340, lng: -121.8835},
+    map: map,
+    animation: google.maps.Animation.DROP,
+    title: 'Philz Coffee',
+    label: 'YOU',
+
+  })
+
+  var SJSUstudentInfoWindow = new google.maps.InfoWindow({
+    content: SJSUstudentMarker.title +'<p>Availability: Busy</p>'
+  });
+
+  SJSUstudentMarker.addListener('click', function() {
+    SJSUstudentInfoWindow.open(map, SJSUstudentMarker);
+  });
+
   var philzCoffeeMarker = new google.maps.Marker({
     position: {lat: 37.3336, lng: -121.8849},
     map: map,
     animation: google.maps.Animation.DROP,
     title: 'Philz Coffee',
     label: 'A',
-    color: "GREEN"
 
   })
 
@@ -57,6 +72,43 @@ function initMap() {
     caffeFrascatiInfoWindow.open(map, caffeFrascatiMarker);
   });
 
+  var academicCoffeeMarker = new google.maps.Marker({
+    position: {lat: 37.32925, lng: -121.88416},
+    map: map,
+    animation: google.maps.Animation.DROP,
+    title: 'Academic Coffee',
+    label: 'D'
+  })
+
+  var academicCoffeeInfoWindow = new google.maps.InfoWindow({
+    content: academicCoffeeMarker.title +'<p>Availability: Busy</p>'
+  });
+
+  academicCoffeeMarker.addListener('click', function() {
+    academicCoffeeInfoWindow.open(map, academicCoffeeMarker);
+  });
+
+
+var properCupMarker = new google.maps.Marker({
+    position: {lat: 37.33516, lng: -121.89212},
+    map: map,
+    animation: google.maps.Animation.DROP,
+    title: 'The Proper Cup',
+    label: {
+    text: 'The Proper Cup',
+    color: "#000000",
+    fontSize: "16px",
+    fontWeight: "bold"
+  }
+  })
+
+  var properCupInfoWindow = new google.maps.InfoWindow({
+    content: properCupMarker.title +'<p>Availability: Free</p>'
+  });
+
+ properCupMarker.addListener('click', function() {
+    properCupInfoWindow.open(map, properCupMarker);
+  });
 
   function toggleBounce() {
     if (marker.getAnimation() !== null) {
