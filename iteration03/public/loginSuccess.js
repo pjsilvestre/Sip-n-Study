33ff94dd74fe687597da65f478e493523ecf23bb
Sachin <sachin.shah@sjsu.edu>
2019-04-17 23:04:47 -0700

    Initialized fields to populate and setup FBUI for html

--- iteration03/public/loginSuccess.js
+++ iteration03/public/loginSuccess.js
@@ -1,3 +1,15 @@
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+//Create a variable to represent the user that is currently logged in 
+var user = firebase.auth().currentUser;
+//variables to store name email and if the email is verified
+var name, email, emailVerified;
+if (user != null) {
+  // User is signed in, display User information
+  name = user.displayName;
+  email = user.email;
+  emailVerified = user.emailVerified;
+} else {
+  // No user is signed in.
+}
