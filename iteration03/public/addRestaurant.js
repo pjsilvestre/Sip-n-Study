52f913c7bb10379abf440fb8d0086e85fc382163
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-04-18 09:48:42 -0700

    Add userUID field to restaurant objects

--- iteration03/public/addRestaurant.js
+++ iteration03/public/addRestaurant.js
@@ -6,11 +6,19 @@ function addRestaurant(){
     var selector = document.getElementById("isBusy");
     var isBusy = selector[selector.selectedIndex].value;
     
-    db.collection("restaurants").add({
-        restaurantName: restaurantName,
-        address: address,
-        isBusy: isBusy
-    })
 
-    alert("Restaurant added!");
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          db.collection("restaurants").add({
+            restaurantName: restaurantName,
+            address: address,
+            isBusy: isBusy,
+            userUID: user.uid
+        })
+          alert("Restaurant added!");
+        } else {
+          alert("Restaurant not added! Were you logged out?")
+        }
+      });
 }
