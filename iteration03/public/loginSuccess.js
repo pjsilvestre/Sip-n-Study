5e5f537a16f275efc757d9f6f743ecd65723cdd4
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-04-18 11:42:00 -0700

    Add check to loginSucess to see if user has restaurant in db

--- iteration03/public/loginSuccess.js
+++ iteration03/public/loginSuccess.js
@@ -14,6 +14,28 @@ function initApp()
   });
 }
 
+var db = firebase.firestore();
+
+//TODO
+function userHasRestaurant(){
+  
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      db.collection("restaurants").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          console.log(doc.data().userUID);
+          if (doc.data().userUID === user.uid)
+          {
+            document.getElementById("addRestaurantPrompt").textContent = '';
+          }
+        });
+      });
+    }
+  });
+}
+
 window.onload = function() {
   initApp();
+  userHasRestaurant();
 }
