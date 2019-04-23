f5b4ee45057afbf6de2bcae8ef7dfd74a32e522e
Sachin <sachin.shah@sjsu.edu>
2019-04-23 01:05:31 -0700

    Added functionality to retrieve live user location

--- iteration04/public/scripts/index.js
+++ iteration04/public/scripts/index.js
@@ -7,7 +7,35 @@ function initMap()
         zoom: 16,
         //disableDefaultUI: true
     });
+    var infoWindow = new google.maps.InfoWindow();
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('My Location');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
 
 function initTable()
 {
