85666bbdaa425dec7ca9b8744097e799b4cf8e67
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-04-23 04:14:00 -0700

    Add new user "add restaurant" prompt

--- iteration04/public/scripts/loginSuccess.js
+++ iteration04/public/scripts/loginSuccess.js
@@ -1,15 +1,32 @@
-function greetUser()
-{
-    firebase.auth().onAuthStateChanged(function(user){
-        if (user) {
-            //User was signed in successfully.
-            var displayName = user.displayName;
-            document.getElementById("user").textContent="Login successful. Welcome, " +user.displayName + ".";
-        }
-    })
+function greetUser() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      //User was signed in successfully.
+      var displayName = user.displayName;
+      document.getElementById("user").textContent =
+        "Login successful. Welcome, " + user.displayName + "!";
+    }
+  });
 }
 
-window.onload = function()
-{
-    greetUser();
+function addRestaurantPrompt() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var db = firebase.firestore();
+      db.collection("restaurants")
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.foreach(function(doc) {
+            if (doc.data().userUID === user.uid) {
+              document.getElementById("addRestaurantPrompt").textContent = ";";
+            }
+          });
+        });
+    }
+  });
 }
+
+window.onload = function() {
+  greetUser();
+  addRestaurantPrompt();
+};
